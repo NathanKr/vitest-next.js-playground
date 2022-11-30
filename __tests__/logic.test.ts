fb94d1106866c096cf707f354b6d8b78853269bf
Nathan Krasney <natankrasney@gmail.com>
2022-11-30 15:27:21 +0200

    ...

--- __tests__/logic.test.ts
+++ __tests__/logic.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { sum } from "src/utils";
+import { sum } from "src/logic/utils";
 
 test("1+2 is 3", () => {
   expect(sum(1, 2)).toBe(3);
---
+++ src/components/counter.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <p>counter : {count}</p>
+    </div>
+  );
+};
+
+export default Counter;
